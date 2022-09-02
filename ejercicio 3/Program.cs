506d6a2424b1772e099a389c5a0694457fb165a7
labinf1 <labinf1@PAS-W-L1PC11>
2022-09-02 17:43:32 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio 3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ejercicio3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            double cuadrado;
+            int numero = 821;
+            cuadrado = Math.Pow(numero, 2);
+            Console.WriteLine($"El resultado del número 821 al cuadrado es : {cuadrado}");
+        }
+    }
+}
---
+++ ejercicio2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ejercicio2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int numero1;
+            int numero2;
+            int numero3;
+            int numero4;
+            int suma;
+            int producto;
+            double media;
+            Console.WriteLine("Ingrese el primer número:");
+            numero1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número:");
+            numero2 = Convert.ToInt32(Console.ReadLine());  
+            Console.WriteLine("Ingrese el tercer número:");
+            numero3=Convert.ToInt32(Console.ReadLine());    
+            Console.WriteLine("Ingrese el cuarto número:");
+            numero4=Convert.ToInt32(Console.ReadLine());
+            suma = numero1 + numero2 + numero3 + numero4;
+            producto = numero1 * numero2 * numero3 * numero4;
+            media = (numero1 + numero2 + numero3 + numero4) / 4;
+            Console.WriteLine($"El resultado de la suma de los números es: {suma}");
+            Console.WriteLine($"El resultado del producto de los números es: {producto}");
+            Console.WriteLine($"El resultado de la media es: {media}");
+        }
+    }
+}
