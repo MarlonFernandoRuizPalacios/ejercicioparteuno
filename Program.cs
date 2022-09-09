211051b024dc6dabe838a378d00ed20d501ea809
MarlonFernandoRuizPalacios <111613832+MarlonFernandoRuizPalacios@users.noreply.github.com>
2022-09-08 19:02:56 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ejercicio5
+{
+    public class program
+    {
+        public static void Main()
+        {
+
+            int limite;
+            double potencia;
+            int numero = 2;
+            int cotador;
+            cotador = 0;
+            limite = 10;
+            for (cotador = 0; cotador <= limite; cotador++)
+            {
+                potencia = Math.Pow(numero, cotador);
+                Console.WriteLine($"El resultado del número 2 elevado a la potencia de {cotador} es : {potencia}");
+            }
+
+        }
+    }
+}
